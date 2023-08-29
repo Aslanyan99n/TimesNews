42f8378c0d4d1d5247dd4c84372c5ef2c653ad50
Aslanyan99n <a99narek@gmail.com>
2023-08-29 07:38:52 +0400

    Initial Commit

---
+++ TimesNews/TimesNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesNewsApp.swift
+//  TimesNews
+//
+//  Created by Narek on 29.08.23.
+//
+
+import SwiftUI
+
+@main
+struct TimesNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
